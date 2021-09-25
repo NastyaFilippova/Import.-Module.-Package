5a89c938152e17d220bb9667bac7e530e13b1db1
Филиппова Анастасия <a.filippova@inside.glowbyte.com>
2021-09-25 15:20:15 +0300

    The file dirty_main is created and changes is added

---
+++ dirty_main.py
@@ -0,0 +1,8 @@
+from application.salary import *
+from application.db.people import *
+
+if __name__ == '__main__':
+    calculate_salary()
+    check_rules()
+    get_employees()
+    new_step()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 from application.salary import calculate_salary
 from application.db.people import get_employees
+from datetime import date
+
+today = date.today()
 
 if __name__ == '__main__':
     calculate_salary()
     get_employees()
-
-
-
-
+    print(f'Today: {today}')
